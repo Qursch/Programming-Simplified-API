856cf9fc2c6fac806b9062dcc897227bd57bab03
mx-mw <87119201+mx-mw@users.noreply.github.com>
2021-11-03 21:53:33 +1100

    basic poggers authentication

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { config } from 'dotenv'
+config();
+const { JWT_SECRET_KEY } = process.env
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor() {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: JWT_SECRET_KEY,
+		});
+	}
+
+	async validate(payload: any) {
+		return { userId: payload.sub, username: payload.username };
+	}
+}
