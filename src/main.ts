f2d807734e1ea2240326604677485c87391ec1da
mx-mw <87119201+mx-mw@users.noreply.github.com>
2021-12-05 00:17:22 +1100

    add helmet support

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
 	config();
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+	app.use(helmet());
 	app.useGlobalPipes(new ValidationPipe());
 	// something im using port 8000 for something else don't switch it
 	await app.listen(process.env.PORT ?? 8080);
