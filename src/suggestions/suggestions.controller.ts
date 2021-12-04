c9457b3dc21479d12237fe29183281ed2f84b60c
mx-mw <87119201+mx-mw@users.noreply.github.com>
2021-12-04 23:56:55 +1100

    added notion support for bug reports

--- src/suggestions/suggestions.controller.ts
+++ src/suggestions/suggestions.controller.ts
@@ -2,12 +2,17 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/guards/auth/jwt.guard';
 import { MessageEmbed } from 'discord.js';
 import axios from 'axios';
+import { Client } from '@notionhq/client';
 
 const WEBHOOK_URI = 'https://discord.com/api/webhooks/916512548000497764/8nRHdkg5s023Of4x2Zq22z-rwD-O5Zk2V8T4D0aREbszJd9CVBaSfIaTGYOW1jNbg3qY';
 export type Feedback = {
     avatarUrl  : string,
     username   : string
 } & Record<string, string>;
+
+const notion = new Client({
+	auth: process.env.NOTION_TOKEN
+});
 
 @Controller('suggestions')
 export class SuggestionsController {
@@ -17,7 +22,7 @@ export class SuggestionsController {
 
 		const url = feedback.avatarUrl;
 		delete feedback.avatarUrl;
-		const username = feedback.avatarUrl;
+		const username = feedback.username;
 		delete feedback.username;
 
 		const keys = Object.keys(feedback);
@@ -43,24 +48,44 @@ export class SuggestionsController {
 	// @UseGuards(JwtAuthGuard)
 	@Post('bug')
 	async bug(@Body() feedback: Feedback) {
-		const url = feedback.avatarUrl;
+		// const url = feedback.avatarUrl;
 		delete feedback.avatarUrl;
+		
+		const username = feedback.username;
+		delete feedback.username;
 		const keys = Object.keys(feedback);
-		const embed = new MessageEmbed()
-			.setTitle(`Bug Report from ${feedback.username}`)
-			.addFields(keys.map(i => {
-				const ret =  { name: i, value: feedback[i] };
-				return ret;
-			}))
-			.setThumbnail(url)
-			.setAuthor(
-				'厨晚欢貌能', 
-				url
-			);
-		axios.post(WEBHOOK_URI, {
-			embeds: [
-				embed
-			]
+
+		const props = {
+			Author: {
+				title: [
+					{
+						text: {
+							content: `Bug Report by ${username ?? '<username not provided>'}`,
+						}
+					}
+				]
+			},
+		};
+
+		keys.forEach(i => {
+			props[i] = {
+				rich_text: [
+					{
+						text: {
+							content: feedback[i]
+						}
+					}
+				]
+			};
 		});
+
+		const res = await notion.pages.create({
+			parent: {
+				database_id: '0ea6a20bacaf400eb261ac128c109c61'
+			},
+			properties: props
+		});
+
+		console.log(res);
 	}
 }
