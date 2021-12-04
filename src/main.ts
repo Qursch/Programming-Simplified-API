cc869c31ac8f8fffe44b12f1a8a6f28edca18c74
mx-mw <87119201+mx-mw@users.noreply.github.com>
2021-12-05 00:22:07 +1100

    Revert "add csurf"
    This reverts commit 47d78c7237eb654194fccc9bc1e0c803c1b8ef5b.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
 	config();
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
 	app.use(helmet());
-	app.use(csurf());
 	app.useGlobalPipes(new ValidationPipe());
 	// something im using port 8000 for something else don't switch it
 	await app.listen(process.env.PORT ?? 8080);
