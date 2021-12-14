d6416dfb9d432bf0097310b74f3e182189cf3a98
Hazim <hazimarafa80@gmail.com>
2021-12-13 19:00:24 -0800

    Update progress.gateway.ts

--- src/course/progress.gateway.ts
+++ src/course/progress.gateway.ts
@@ -1,5 +1,5 @@
 import { Req, UseGuards } from '@nestjs/common';
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WsResponse } from '@nestjs/websockets';
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import LessonProgressDto from 'src/dto/progress.dto';
 import { JwtAuthGuard } from 'src/guards/auth/jwt.guard';
 import { CourseService } from './course.service';
@@ -13,6 +13,7 @@ export class ProgressGateway implements OnGatewayConnection, OnGatewayDisconnect
 		// client.disconnect();
 		console.log(`Client connected: ${client.id}`);
 	}
+
 	handleDisconnect(client: Socket) {
 		console.log(`Client disconnected: ${client.id}`);
 	}
