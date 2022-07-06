0e7a15c6a7c5cf5f6ba68f0342cefa323a939b0c
Qursch <qursch@outlook.com>
2022-07-06 13:58:14 -0400

    Fix formatting

--- src/codepost/codepost.controller.ts
+++ src/codepost/codepost.controller.ts
@@ -2,13 +2,12 @@ import { Controller, UseGuards, Get, Req, Param } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/guards/auth/jwt.guard';
 import { CodepostService } from 'src/codepost/codepost.service';
 
-
 @Controller('codepost')
 export class CodepostController {
-	constructor(private codepostService: CodepostService) { }
+	constructor(private codepostService: CodepostService) {}
 
-	@UseGuards(JwtAuthGuard)
-	@Get('registered/:courseId')
+  @UseGuards(JwtAuthGuard)
+  @Get('registered/:courseId')
 	async isRegistered(@Req() req, @Param('courseId') courseId: string) {
 		return this.codepostService.isRegistered(req.user.email, courseId);
 	}
--- src/codepost/codepost.service.ts
+++ src/codepost/codepost.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable, NotFoundException } from '@nestjs/common';
 import axios from 'axios';
 import { CourseService } from 'src/course/course.service';
@@ -6,28 +5,30 @@ import { CourseService } from 'src/course/course.service';
 const options = {
 	headers: {
 		'Content-Type': 'application/json',
-		'Accept': 'application/json',
-		'Authorization': 'Token ' + process.env.CODEPOST_API_KEY,
+		Accept: 'application/json',
+		Authorization: 'Token ' + process.env.CODEPOST_API_KEY,
 	},
 };
 
 @Injectable()
 export class CodepostService {
-	constructor(private courseService: CourseService) { }
+	constructor(private courseService: CourseService) {}
 
 	public async isRegistered(email: string, courseId: string) {
 		try {
-			const response = await axios.get(`https://api.codepost.io/courses/${courseId}/roster`, options);
-			if(response.data.students.some(user => user == email.toLowerCase())) {
+			const response = await axios.get(
+				`https://api.codepost.io/courses/${courseId}/roster`,
+				options,
+			);
+			if (response.data.students.some((user) => user == email.toLowerCase())) {
 				return true;
 			}
-			
+
 			const course = await this.courseService.findOne_Course(courseId);
-			if(course.codePostInvite) {
+			if (course.codePostInvite) {
 				return 'https://codepost.io/signup/join?code=' + course.codePostInvite;
 			}
 			return false;
-			
 		} catch (error) {
 			throw new NotFoundException('Course not found.');
 		}
